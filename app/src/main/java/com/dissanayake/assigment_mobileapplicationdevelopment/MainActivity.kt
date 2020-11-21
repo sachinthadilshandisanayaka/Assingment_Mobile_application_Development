57ca4248589d45f9a0c8ed2ed55b3b6ee1750ad2
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-21 16:44:46 +0530

    begin

---
+++ app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dissanayake.assigment_mobileapplicationdevelopment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
