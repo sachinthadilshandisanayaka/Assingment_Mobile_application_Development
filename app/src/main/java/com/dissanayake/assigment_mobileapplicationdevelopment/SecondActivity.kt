6b88ffe19261c70a980a660dc7e61b227bab4397
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-24 17:26:25 +0530

    updated

--- app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/SecondActivity.kt
+++ app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/SecondActivity.kt
@@ -1,13 +1,18 @@
 package com.dissanayake.assigment_mobileapplicationdevelopment
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.dissanayake.assigment_mobileapplicationdevelopment.post.api.PostApi
 import com.dissanayake.assigment_mobileapplicationdevelopment.post.models.Post
+import com.google.gson.GsonBuilder
+import kotlinx.android.synthetic.main.activity_second2.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SecondActivity : AppCompatActivity() {
@@ -15,8 +20,13 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second2)
 
+        // gson is a decoder
+        val gson = GsonBuilder()
+                .create()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
         var retro = retrofit.create(PostApi::class.java)
@@ -25,6 +35,8 @@ class SecondActivity : AppCompatActivity() {
         postCall.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
                 var title = response.body()!!.title
+                Log.d("second activity", title)
+                textView2.text = title.toString()
             }
 
             override fun onFailure(call: Call<Post>, t: Throwable) {
