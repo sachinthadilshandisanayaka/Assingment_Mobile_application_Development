7e58ad46408e8eddf6bba589c514df9753a6aaf1
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-24 19:14:09 +0530

    udated

--- app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/SecondActivity.kt
+++ app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/SecondActivity.kt
@@ -5,13 +5,11 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.dissanayake.assigment_mobileapplicationdevelopment.post.api.PostApi
+import com.dissanayake.assigment_mobileapplicationdevelopment.post.api.PostApiAll
 import com.dissanayake.assigment_mobileapplicationdevelopment.post.models.Post
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_second2.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 
@@ -30,13 +28,29 @@ class SecondActivity : AppCompatActivity() {
             .build()
 
         var retro = retrofit.create(PostApi::class.java)
+        var retroAll = retrofit.create(PostApiAll::class.java)
         var postCall = retro.posts
+        var postAllCall = retroAll.allPosts
         // api class if success then do this, otherwise it was do not
         postCall.enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
-                var title = response.body()!!.title
-                Log.d("second activity", title)
-                textView2.text = title.toString()
+
+                // for noe item
+//                var title = response.body()!!.title
+//                Log.d("second activity", title)
+//                textView2.text = title.toString()
+
+                // for all items
+
+                val allPostData = response.body()
+                allPostData.forEach { data ->
+                    val context = ""
+                    context + = "userId :" + data.userId + "\n"
+                    context + = "id :" + data.id+ "\n"
+                    context + = "title :" + data.title+ "\n"
+                    context + = "body" + data.body+ "\n"
+                    textView2.append(context)
+                }
             }
 
             override fun onFailure(call: Call<Post>, t: Throwable) {
---
+++ app/src/main/java/com/dissanayake/assigment_mobileapplicationdevelopment/post/api/PostApiAll.java
@@ -0,0 +1,14 @@
+package com.dissanayake.assigment_mobileapplicationdevelopment.post.api;
+
+import com.dissanayake.assigment_mobileapplicationdevelopment.post.models.Post;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostApiAll {
+
+    @GET("/posts")
+    Call<List<Post>> getAllPosts();
+}
